cff179185a527006c8705de8221dbb88707321da
Conrad Dean <conrad.p.dean@gmail.com>
2013-01-07 10:46:07 -0600

    adding api key and auth example file

--- example.py
+++ example.py
@@ -1,10 +1,12 @@
 import requests
+import secure as s
 payload = {
     'page': '1',
     'pagesize': '2',
     'order': 'asc',
     'sort': 'activity',
-    'site': 'stackoverflow'}
+    'site': 'stackoverflow',
+    'key': s.key}
 
 r = requests.get("https://api.stackexchange.com/2.1/questions",
                  params=payload)
---
+++ secure_example.py
@@ -0,0 +1,6 @@
+# rename this to 'secure.py'
+# get your app data from http://stackapps.com/apps/oauth
+
+client_id = 1061
+secret = "your-secret"
+key = "your-key"
