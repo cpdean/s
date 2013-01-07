3c85bcc9812279995ea44dc259a4970416a8dac2
Conrad Dean <conrad.p.dean@gmail.com>
2013-01-07 10:30:05 -0600

    convert querystring to dict

--- example.py
+++ example.py
@@ -1,5 +1,13 @@
 import requests
-r = requests.get("https://api.stackexchange.com/2.1/questions?page=1&pagesize=2&order=asc&sort=activity&site=stackoverflow")
+payload = {
+    'page': '1',
+    'pagesize': '2',
+    'order': 'asc',
+    'sort': 'activity',
+    'site': 'stackoverflow'}
+
+r = requests.get("https://api.stackexchange.com/2.1/questions",
+                 params=payload)
 
 print r.headers
 
